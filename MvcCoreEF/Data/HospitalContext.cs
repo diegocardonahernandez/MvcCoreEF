c450eef1a97770541df118184cc626888d4ad5cb
diegocardonahernandez <diego.cardonahernandez@tajamar365.com>
2026-02-09 14:23:03 +0100

    Add project files.

---
+++ MvcCoreEF/Data/HospitalContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreEF.Models;
+
+namespace MvcCoreEF.Data
+{
+    public class HospitalContext : DbContext
+    {
+        //El constructor recibira siempre las opciones para este contexto
+        //La clase que recibe es DbContextOptions<Context>
+
+        //Estas options debemos enviarla a la clase BASE/SUPER del Dbcontext
+
+        public HospitalContext(DbContextOptions<HospitalContext> options) : base(options)
+        {
+
+        }
+
+        //Debemos tener un colecicon por cada model
+        //Dicha coleccion dee ser Tipo<T>
+        public DbSet<Hospital> hospitales {get;set;}
+
+    }
+}
---
+++ MvcCoreEF/Repositories/RepositoryHospital.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Identity.Client;
+using MvcCoreEF.Data;
+using MvcCoreEF.Models;
+
+namespace MvcCoreEF.Repositories
+{
+    public class RepositoryHospital
+    {
+        private HospitalContext context;
+        public RepositoryHospital(HospitalContext context)
+        {
+            this.context = context;
+
+        }
+
+        public List<Hospital> GetHospitales()
+        {
+            var consulta = from datos in this.context.hospitales select datos;
+            return consulta.ToList();
+        }
+
+    }
+}
